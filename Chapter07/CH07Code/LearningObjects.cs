34c1fedb4410482b42bf13d63751779494b01a78
ralph14 <ralphr@packtpub.com>
2017-12-22 10:50:46 +0530

    Add files via upload

---
+++ Chapter07/CH07Code/LearningObjects.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LearningObjects : MonoBehaviour {
+
+
+	public Person man;
+	public Person woman;
+
+
+
+	void Start() {
+
+
+		man = new Person();
+		man.firstName = "Greg";
+		man.lastName = "Lukosek";
+
+		woman = new Person();
+		woman.firstName = "Kate";
+		woman.lastName = "Lukosek";
+
+		man.spouse = woman;
+		woman.spouse = man;
+
+
+		if (man.IsMarriedWith(woman)) {
+			Debug.Log(man.firstName + " is married to " + woman.firstName);
+		}
+		else {
+			Debug.Log(man.firstName + " and " + woman.firstName + " are not married");
+		}
+	}
+
+}
+
+
+
+
